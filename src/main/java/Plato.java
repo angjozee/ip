4ec88874afc913e1f49f8d0f2460a6bd06ef0881
Jo Zee <jozeeang@gmail.com>
2025-01-20 15:48:02 +0800

    Add Echo functionality to Plato
    - Implemented functionality to echo user commands
    - Exit condition updated to recognise "Farewell" as the termination command

--- src/main/java/Plato.java
+++ src/main/java/Plato.java
@@ -1,9 +1,22 @@
+import java.util.Scanner;
+
 public class Plato {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
         System.out.println("______________________________________");
         System.out.println("Greetings, I am Plato. Nice to meet you!");
         System.out.println("What would you like to do?");
         System.out.println("______________________________________");
-        System.out.println("Farewell. Hope to see you soon!");
+
+        while (sc.hasNext()) {
+            String userInput = sc.nextLine();
+            System.out.println("______________________________________");
+            System.out.println("     " + userInput);
+            System.out.println("______________________________________");
+            if (userInput.equals("Farewell")) {
+                System.out.println("Farewell. Hope to see you soon!");
+                break;
+            }
+        }
     }
 }
