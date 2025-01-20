a1b05b47c20f391e064ba7d18b59bfc96e2a9b17
angjozee <jozeeang@gmail.com>
2025-01-20 15:18:58 +0800

    Rename Duke to Plato and implement custom chatbot greeting
    - Replaced the `Duke` chatbot with a new chatbot named `Plato`.
    - Updated the greeting and farewell messages to align with Plato's unique personality.
    - Removed the Duke ASCII logo and all references to Duke.
    - Updated horizontal lines

---
+++ src/main/java/Plato.java
@@ -0,0 +1,9 @@
+public class Plato {
+    public static void main(String[] args) {
+        System.out.println("______________________________________");
+        System.out.println("Greetings, I am Plato. Nice to meet you!");
+        System.out.println("What would you like to do?");
+        System.out.println("______________________________________");
+        System.out.println("Farewell. Hope to see you soon!");
+    }
+}
