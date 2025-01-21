69c928e1d2b3ae4ad30c4b3fe30fbcf0dce1dd0a
Jo Zee <jozeeang@gmail.com>
2025-01-21 14:49:50 +0800

    Add mark and unmark functionality to Plato chatbot
    - Implemented 'mark <index>' command to mark tasks as done.
    - Implemented 'unmark <index>' command to mark tasks as not done.
    - Enhanced tasks? command to display tasks with status icons ([X] for done, [ ] for not done).
    - Introduced 'Task' class to represent tasks with properties:
      - 'description': Task details.
      - 'isDone': Completion status.
      - Methods: 'markAsDone()', 'markAsNotDone()', 'getStatusIcon()'.

--- src/main/java/Plato.java
+++ src/main/java/Plato.java
@@ -10,28 +10,51 @@ public class Plato {
         System.out.println("What would you like to do?");
         System.out.println("______________________________________");
 
-        List<String> tasks = new ArrayList<>();
+        List<Task> tasks = new ArrayList<>();
 
         while (sc.hasNext()) {
-            String userInput = sc.nextLine();
-            if (userInput.equals("Farewell")) {
+            Task task = new Task(sc.nextLine());
+            if (task.getDescription().equals("Farewell")) {
                 System.out.println("______________________________________");
                 System.out.println("Farewell. Hope to see you soon!");
                 System.out.println("______________________________________");
                 break;
             }
-            if (userInput.equals("tasks?")) {
+            else if (task.getDescription().equals("tasks?")) {
                 System.out.println("______________________________________");
                 System.out.println("Here is your list of tasks: ");
                 for (int i = 0; i < tasks.size(); i++) {
-                    System.out.println((i + 1) + ". " + tasks.get(i));
+                    System.out.println((i + 1) + ". " + "[" + tasks.get(i).getStatusIcon()
+                            + "] " + tasks.get(i).getDescription());
                 }
                 System.out.println("______________________________________");
             }
-            else {
-                tasks.add(userInput);
+            else if (task.getDescription().startsWith("mark")) {
+                String[] parts = task.getDescription().split(" ");
+                int id = Integer.parseInt(parts[1]) - 1;
+                Task tobemarked = tasks.get(id);
                 System.out.println("______________________________________");
-                System.out.println("I have thus added this task: " + userInput);
+                System.out.println("Well done. Marking this task as done.");
+                tobemarked.markAsDone();
+                System.out.println("[" + tobemarked.getStatusIcon() + "] " +
+                        tobemarked.getDescription());
+                System.out.println("______________________________________");
+            }
+            else if (task.getDescription().startsWith("unmark")) {
+                String[] parts = task.getDescription().split(" ");
+                int id = Integer.parseInt(parts[1]) - 1;
+                Task tobeunmarked = tasks.get(id);
+                System.out.println("______________________________________");
+                System.out.println("Alright. Marking this task as not done.");
+                tobeunmarked.markAsNotDone();
+                System.out.println("[" + tobeunmarked.getStatusIcon() + "] " +
+                        tobeunmarked.getDescription());
+                System.out.println("______________________________________");
+            }
+            else {
+                tasks.add(task);
+                System.out.println("______________________________________");
+                System.out.println("I have thus added this task: " + task.getDescription());
                 System.out.println("______________________________________");
             }
         }
