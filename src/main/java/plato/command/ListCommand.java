a166e4ed5497c1313e598461cd0a0265536c5323
Jo Zee <jozeeang@gmail.com>
2025-02-17 15:35:58 +0800

    Add JavaDoc comments for ListCommand.java

--- src/main/java/plato/command/ListCommand.java
+++ src/main/java/plato/command/ListCommand.java
@@ -5,7 +5,19 @@ import plato.model.TaskList;
 import plato.storage.Storage;
 import plato.ui.Ui;
 
+/**
+ * Represents a command to list all tasks in the task list.
+ */
 public class ListCommand extends Command {
+
+    /**
+     * Executes the command by displaying all tasks in the task list.
+     *
+     * @param tasks   The task list containing the user's tasks.
+     * @param ui      The user interface to display messages.
+     * @param storage The storage system (not used in this command but required for consistency).
+     * @throws PlatoException If an error occurs during execution (not expected for this command).
+     */
     @Override
     public void execute(TaskList tasks, Ui ui, Storage storage) throws PlatoException {
         if (tasks.size() == 0) {
